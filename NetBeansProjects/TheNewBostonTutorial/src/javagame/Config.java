3a556d7b7014462f85767a02b7ed10252249760a
LeaL33 <lea.lafon@etu.u-bordeaux.fr>
2016-06-24 11:13:15 +0200

    Suppr conflits

--- NetBeansProjects/TheNewBostonTutorial/src/javagame/Config.java
+++ NetBeansProjects/TheNewBostonTutorial/src/javagame/Config.java
@@ -15,12 +15,7 @@ import org.newdawn.slick.state.*;
  */
 public class Config extends BasicGameState {
     
-<<<<<<< HEAD
     public Config(int state) {
-        
-=======
-    public Config(int instr) {
->>>>>>> 9e3c3dab22ec2314d27dd9dda475512e79357e87
     }
     
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
@@ -28,24 +23,13 @@ public class Config extends BasicGameState {
     }
     
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-<<<<<<< HEAD
         g.drawString("config loul", 50, 50);
-=======
-        
->>>>>>> 9e3c3dab22ec2314d27dd9dda475512e79357e87
     }
     
     public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
         
     }
-<<<<<<< HEAD
-    
-    public int getID() {
-        return 3;
-    }
-=======
->>>>>>> 9e3c3dab22ec2314d27dd9dda475512e79357e87
-    
+
     public int getID() {
         return 3;
     }
