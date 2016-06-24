a37f3a0519fe8ee63ffec742def174d087260565
c-virum <corentin.virumbrales@etu.u-bordeaux.fr>
2016-06-24 11:58:59 +0200

    nouveaux boutons

--- NetBeansProjects/TheNewBostonTutorial/src/javagame/Menu.java
+++ NetBeansProjects/TheNewBostonTutorial/src/javagame/Menu.java
@@ -6,12 +6,6 @@ import org.newdawn.slick.state.*;
 
 public class Menu extends BasicGameState {
     
-    // public String mouse = "No input yet!";
-    /*
-    int imagex = 200;
-    int imagey = 200;
-    */
-    
     class Button {
         Image image, imageActive;
         int x1, x2, y1, y2;
@@ -52,15 +46,7 @@ public class Menu extends BasicGameState {
     }
     
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-        /*
-        g.drawString(mouse, 50, 50);
-        g.drawRect(50, 100, 60, 120);
-        Image baboon = new Image("res/baboon.png");
         
-        //g.drawImage(baboon, imagex, imagey);
-        g.fillOval(75, 100, 100, 100);
-        g.drawString("Play Now!", 80, 70);
-        */
         g.drawImage(imgMenu, 0, 0);
         
         if (buttonPlay.active) {
@@ -87,46 +73,11 @@ public class Menu extends BasicGameState {
     }
     
     public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
-        /*
-        int xpos = Mouse.getX();
-        int ypos = Mouse.getY();
-        mouse = "Mouse position x: " + xpos  + " y: " + ypos;
         
-        Input input = gc.getInput();
-        if (input.isKeyDown(Input.KEY_UP)) {
-            imagey -= 1;
-        }
-        if (input.isKeyDown(Input.KEY_DOWN)) {
-            imagey += 1;
-        }
-        if (input.isKeyDown(Input.KEY_LEFT)) {
-            imagex -= 1;
-        }
-        if (input.isKeyDown(Input.KEY_RIGHT)) {
-            imagex += 1;
-        }
-        */
         Input input = gc.getInput();
         int xpos = Mouse.getX();
         int ypos = 480-Mouse.getY();
         
-        /*
-        if ((xpos > 260 && xpos < 380) && (ypos > 180 && ypos <230)) {
-            if (input.isMouseButtonDown(0)) {
-                sbg.enterState(1);
-            }
-        }
-        if ((xpos > 170 && xpos < 475) && (ypos > 115 && ypos <155)) {
-            if (input.isMouseButtonDown(0)) {
-                System.out.println("Instructions pressed");
-            }
-        }
-        if ((xpos > 216 && xpos < 426) && (ypos > 48 && ypos <98)) {
-            if (input.isMouseButtonDown(0)) {
-                System.out.println("Settings pressed");
-            }
-        }
-        */
         if (buttonPlay.hover(xpos, ypos)) {
             buttonPlay.active = true;
             if (input.isMouseButtonDown(0)) {
