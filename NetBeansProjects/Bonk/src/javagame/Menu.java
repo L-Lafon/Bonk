ea3d40deb959631bed198f1a42b3b9012d2d32db
LeaL33 <lea.lafon@etu.u-bordeaux.fr>
2016-07-07 15:52:29 +0200

    Javadoc class menu

--- NetBeansProjects/Bonk/src/javagame/Menu.java
+++ NetBeansProjects/Bonk/src/javagame/Menu.java
@@ -4,50 +4,79 @@ import org.lwjgl.input.Mouse;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
+/**
+ * Menu Class for the beggining of the game : play, instructions, ...
+ * @author lea & corentin
+ */
 public class Menu extends BasicGameState {
-    
+    /**
+     * Menu class is extended to BasicGameState class into Slick
+     */
     class Button {
         Image image, imageActive;
         int x1, x2, y1, y2;
         boolean active = false;
-        
-        public Button(Image image, Image imageActive, int x1, int y1, int x2, int y2) {
+        /**
+         * Button method display button with 2 image which can switch 
+         * at the positions x1 and y1 entered 
+         * @param image
+         * @param imageActive
+         * @param x1
+         * @param y1 
+         */
+        public Button(Image image, Image imageActive, int x1, int y1) {
             this.image = image;
             this.imageActive = imageActive;
             this.x1 = x1;
             this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
+            this.x2 = x1 + this.image.getWidth();
+            this.y2 = y1 + this.image.getHeight();
         }
-        
+        /**
+         * Hover method checks if the mouse pointer is on the button
+         * @param xpos
+         * @param ypos
+         * @return true if the mouse pointer is on the button, else false
+         */
         public boolean hover(int xpos, int ypos) {
-            if (xpos > this.x1 && xpos < this.x2 && ypos > this.y1 && ypos < this.y2) {
-                return true;
-            }
-            else {
-                return false;
-            }
+            return xpos > this.x1 && xpos < this.x2 && ypos > this.y1 && ypos < this.y2;
         }
     }
     
     Image bgMenu;
     Button buttonPlay, buttonInstr, buttonSettings, buttonSound;
     
+    /**
+     * 
+     * @param state 
+     */
     public Menu(int state) {
         
     }
     
+    /**
+     * Initialise the backgroung and buttons used
+     * @param gc
+     * @param sbg
+     * @throws SlickException 
+     */
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
         bgMenu = new Image("res/title_new.png");
         
-        buttonPlay = new Button(new Image("res/Play.png"), new Image("res/PlayActive.png"),128,256,512,320);
-        buttonInstr = new Button(new Image("res/Instructions.png"), new Image("res/InstructionsActive.png"),128,320,512,384);
-        buttonSettings = new Button(new Image("res/Settings.png"), new Image("res/SettingsActive.png"),128,384,512,448);
-        buttonSound = new Button(new Image("res/SoundOff.png"), new Image("res/SoundOn.png"), 500, 30, 600, 130);
+        buttonPlay = new Button(new Image("res/Play.png"), new Image("res/PlayActive.png"),128,256);
+        buttonInstr = new Button(new Image("res/Instructions.png"), new Image("res/InstructionsActive.png"),128,320);
+        buttonSettings = new Button(new Image("res/Settings.png"), new Image("res/SettingsActive.png"),128,384);
+        buttonSound = new Button(new Image("res/SoundOff.png"), new Image("res/SoundOn.png"), 500, 30);
     }
     
+    /**
+     * Draws everything on screen
+     * @param gc
+     * @param sbg
+     * @param g
+     * @throws SlickException 
+     */
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-        
         g.drawImage(bgMenu, 0, 0);
         
         if (buttonPlay.active) {
@@ -76,11 +105,16 @@ public class Menu extends BasicGameState {
         } else {
             g.drawImage(buttonSound.image, buttonSound.x1, buttonSound.y1);
         }
-        
     }
     
+    /**
+     * Updates all the variables
+     * @param gc the game container
+     * @param sbg the state based games
+     * @param delta the time spent since the last call of the update method
+     * @throws SlickException 
+     */
     public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
-        
         Input input = gc.getInput();
         int xpos = Mouse.getX();
         int ypos = 480-Mouse.getY();
@@ -127,6 +161,10 @@ public class Menu extends BasicGameState {
         }
     }
     
+    /**
+     * get the id state of the class
+     * @return 0
+     */
     public int getID() {
         return 0;
     }
